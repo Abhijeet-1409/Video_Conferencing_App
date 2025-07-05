8e2841a794eb5aeb53a4d085b76c8a0e8dda2cb4
Abhijeet-1409 <abhijeetjoy14@gmail.com>
2025-07-05 14:50:03 +0530

    Add AccessDenied Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import './App.css';
 import { createBrowserRouter, RouterProvider ,Link} from 'react-router-dom';
 import HomePage from './pages/home/HomePage';
 import JoinPage from './pages/join/JoinPage';
+import AccessDeniedPage from './pages/error/AccessDeniedPage';
 import AppContextProvider from './store/app-context';
 import { AppContext } from './store/app-context';
 import { useContext } from 'react';
@@ -12,19 +13,11 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 const clientId = import.meta.env.VITE_CLIENT_ID;
 
 function App() {
-  
+
   const  SecureJoin  = ()=> {
     const {userData,roomId} = useContext(AppContext);
-      return ( userData && roomId.length > 0  ) ? <JoinPage/> : (
-        <>
-           <div>
-            <h2>
-               You are not signIn , please sign in go , to <Link to='/'>Home</Link>
-            </h2>
-           </div>
-        </>
-      ) ;
-  }; 
+      return ( userData && roomId.length > 0  ) ? <JoinPage/> : <AccessDeniedPage/> ;
+  };
 
   const router = createBrowserRouter([
     {
@@ -42,7 +35,7 @@ function App() {
        <RouterProvider router={router} />
      </AppContextProvider>
      </GoogleOAuthProvider>
-   
+
   )
 }
 
