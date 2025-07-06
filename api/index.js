25e11a40c44d93e0e3fb6ec927dad0647a7090a7
Abhijeet-1409 <abhijeetjoy14@gmail.com>
2025-07-07 01:04:06 +0530

    Feat: switch to hybrid WebRTC backend with MongoDB support

--- api/index.js
+++ api/index.js
@@ -9,6 +9,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server);
 const mongoose = require('mongoose');
+const crypto = require('crypto');
 
 const MONGOUSERNAME = process.env.MONGOUSERNAME;
 const MONGOPASSWORD = process.env.MONGOPASSWORD;
@@ -16,22 +17,22 @@ const MONGODBNAME = process.env.MONGODBNAME;
 
 const URI = `mongodb+srv://${MONGOUSERNAME}:${MONGOPASSWORD}@cluster0.pnufpgg.mongodb.net/${MONGODBNAME}?retryWrites=true&w=majority&appName=Cluster0`;
 
-const rooms = [
+const Room = require('./models/Room');
+const Participant = require('./models/Participant');
+const Message = require('./models/Message');
+
+let inMemoryParticipants = [
   /*
   {
-   roomId : string,
-   participants : [
-    {
-      socketId : string,
-      userData : {},
-      ICE_Candidates : [],
-    }
-   ]
+    roomId: string
+    socketId : string,
+    userData : {},
+    ICE_Candidates : [],
   }
- */
-];
+  */
+]
 
-const offers = [
+let inMemoryOffers = [
   /*
    {
      responded : bool,
@@ -43,7 +44,7 @@ const offers = [
   */
 ]
 
-const answers = [
+let inMemoryAnswers = [
   /*
    {
      responded : bool,
@@ -55,7 +56,17 @@ const answers = [
   */
 ]
 
-
+// Generate unique roomId
+async function generateUniqueRoomId() {
+  let roomId;
+  let exists = true;
+  while (exists) {
+    roomId = crypto.randomBytes(4).toString('hex');
+    const room = await Room.findOne({ roomId });
+    exists = !!room;
+  }
+  return roomId;
+}
 
 app.use(cors());
 app.use(express.json());
@@ -67,97 +78,161 @@ app.get('/', (req, res) => {
 io.on("connection", (socket) => {
 
 
-  socket.on('createRoom', () => {
+  socket.on('createRoom', async () => {
     console.log("createRoom");
-    const roomId = Math.floor(Math.random() * 1000000).toString();
-    rooms.push({ roomId, 'participants': [] });
-    socket.emit('roomCreatedSuccessfully', roomId);
-  });
-
-  socket.on('joinRoom', ({ roomId, userData }) => {
-    console.log("joinRoom");
-    const room = rooms.find((room) => room.roomId === roomId);
-    if (room) {
-      socket.join(roomId);
-      room.participants.push({ socketId: socket.id, userData, ICE_Candidates: [] });
-      socket.broadcast.to(roomId).emit('newUserJoined', { roomParticipants: room.participants, remoteSocketId: socket.id, remoteUserData: userData });
-      socket.emit('joinRoomSuccessfully', room.participants);
-    } else {
-      socket.emit('joinRoomUnSuccessful', `no room with id ${roomId} found`);
+    try {
+      console.log("createRoom");
+      const roomId = await generateUniqueRoomId();
+      await Room.create({ roomId, participants: [] });
+      socket.emit('roomCreatedSuccessfully', roomId);
+    }
+    catch (error) {
+      console.error("Error creating room:", error.message);
     }
   });
 
-  socket.on("message_Sent", ({ roomId, msgObj }) => {
+  socket.on('joinRoom', async ({ roomId, userData }) => {
+    console.log("joinRoom");
+    try {
+      const room = await Room.findOne({ roomId });
+
+      if (!room) {
+        socket.emit('joinRoomUnSuccessful', `no room with id ${roomId} found`);
+        return
+      }
+
+      socket.join(roomId)
+
+      inMemoryParticipants.push({
+        socketId: socket.id,
+        userData,
+        roomId,
+        ICE_Candidates: [],
+      })
+      const roomParticipants = inMemoryParticipants.filter(inMemoParticpant => inMemoParticpant.roomId === roomId);
+
+      socket.broadcast.to(roomId).emit('newUserJoined', {
+        roomParticipants,
+        remoteSocketId: socket.id,
+        remoteUserData: userData
+      });
+      socket.emit('joinRoomSuccessfully', roomParticipants);
+
+      const participant = await Participant.create({
+        socketId: socket.id,
+        userData,
+        roomId,
+      })
+      room.participants.push(participant._id);
+      await room.save();
+
+    } catch (error) {
+      console.error('Join room error:', error.message);
+    }
+  });
+
+  socket.on("message_Sent", async ({ roomId, msgObj }) => {
     console.log("message_Sent");
-    socket.to(roomId).emit('message_Recieved', msgObj);
+    try {
+      socket.to(roomId).emit('message_Recieved', msgObj);
+      const { info, senderName } = msgObj;
+      await Message.create({ roomId, info, senderName });
+    } catch (error) {
+      console.error('Message sent error:', error.message);
+    }
   });
 
   socket.on("send_ICE_Candidate", ({ roomId, userData, iceCandidate }) => {
-    // console.log("send_ICE_Candidate");
+    console.log("send_ICE_Candidate");
     let msg = `there is no user with socketId : ${socket.id} and userName : ${userData.name} to add ICE Candidate`;
-    const room = rooms.find(room => room.roomId === roomId);
-    const user = room.participants.find(user => (user.socketId === socket.id && user.userData.id === userData.id));
-    if (user) {
-      user.ICE_Candidates.push(iceCandidate);
+    const participant = inMemoryParticipants.find(
+      inMemoParticpant => (
+        inMemoParticpant.roomId === roomId &&
+        inMemoParticpant.socketId === socket.id
+      )
+    );
+    if (participant) {
+      participant.ICE_Candidates.push(iceCandidate);
       msg = `ICE Candidate add Successfully`;
       socket.to(roomId).emit("add_ice_candidate", { remoteSocketId: socket.id, iceCandidate });
     }
     socket.emit("received_ICE_Candidate", msg);
-
   });
 
   socket.on("offer", ({ roomId, offer, receiver_SocketId }) => {
     console.log("offer");
     const offerObj = { responded: false, roomId, offer, sender_SocketId: socket.id, receiver_SocketId };
-    offers.push(offerObj);
+    inMemoryOffers.push(offerObj);
     io.to(receiver_SocketId).emit('received_offer_for_you', offerObj);
   });
 
   socket.on("answer", ({ roomId, answer, receiver_SocketId }, ackFunc) => {
     console.log("answer");
-    const room = rooms.find(room => room.roomId === roomId);
-    const user = room.participants.find(user => user.socketId === receiver_SocketId);
+    const participant = inMemoryParticipants.find(
+      inMemoParticpant => (
+        inMemoParticpant.roomId === roomId &&
+        inMemoParticpant.socketId === receiver_SocketId
+      )
+    );
     const answerObj = { responded: false, roomId, answer, receiver_SocketId, sender_SocketId: socket.id };
-    answers.push(answerObj);
-    ackFunc(user.ICE_Candidates);
+    inMemoryAnswers.push(answerObj);
+    ackFunc(participant?.ICE_Candidates || []);
     io.to(receiver_SocketId).emit('received_answer_for_you', answerObj);
   });
 
   socket.on("ask_For_ICE_Candidate", ({ roomId, receiver_SocketId }, ackFunc) => {
     console.log("ask_For_ICE_Candidate");
-    const room = rooms.find(room => room.roomId === roomId);
-    const user = room.participants.find(user => user.socketId === receiver_SocketId);
-    ackFunc(user.ICE_Candidates);
+    const participant = inMemoryParticipants.find(
+      inMemoParticpant => (
+        inMemoParticpant.roomId === roomId &&
+        inMemoParticpant.socketId === receiver_SocketId
+      )
+    );
+    ackFunc(participant?.ICE_Candidates || []);
   });
 
 
   socket.on("LeaveRoom", ({ roomId }) => {
     console.log("leaveRoom");
-    const room = rooms.find(room => room.roomId === roomId);
-    if (room) {
-      room.participants = room.participants.filter(participant => participant.socketId !== socket.id);
-      socket.broadcast.to(roomId).emit("participantLeave", { participants: room.participants, leaveSocketId: socket.id });
-      socket.leave(roomId);
-    }
+    const roomParticipants = inMemoryParticipants.filter(
+      inMemoParticpant => (
+        inMemoParticpant.roomId === roomId &&
+        inMemoParticpant.socketId !== socket.id
+      )
+    );
+    inMemoryParticipants = inMemoryParticipants.filter(inMemoParticpant => inMemoParticpant.socketId !== socket.id);
+    inMemoryOffers = inMemoryOffers.filter(inMemoOfferObj => inMemoOfferObj.sender_SocketId !== socket.id);
+    inMemoryAnswers = inMemoryAnswers.filter(inMemoAnswerObj => inMemoAnswerObj.sender_SocketId !== socket.id);
+    socket.broadcast.to(roomId).emit("participantLeave", { participants: roomParticipants, leaveSocketId: socket.id });
+    socket.leave(roomId);
   });
 
   socket.on("offer_Responded", (offerObj) => {
     console.log("offer_Responded");
     let msg = 'offer is not updated';
-    const actualOffer = offers.find(offerObject => (offerObject.roomId === offerObj.roomId && offerObject.sender_SocketId == offerObj.sender_SocketId));
-    if (actualOffer?.roomId?.length > 0) {
+    const actualOffer = inMemoryOffers.find(
+      inMemoOfferObj => (
+        inMemoOfferObj.roomId === offerObj.roomId &&
+        inMemoOfferObj.sender_SocketId == offerObj.sender_SocketId
+      )
+    );
+    if (actualOffer) {
       msg = 'offer is updated';
       actualOffer.responded = true;
     }
     socket.emit("offer_Update", msg);
-
   });
 
   socket.on("answer_Responded", (answerObj) => {
     console.log("answer_Responded");
     let msg = 'answer is not updated';
-    const actualAnswer = answers.find(answerObject => (answerObject.roomId === answerObj.roomId && answerObject.sender_SocketId === answerObj.sender_SocketId));
-    if (actualAnswer?.roomId?.length > 0) {
+    const actualAnswer = inMemoryAnswers.find(
+      inMemoAnswerObj => (
+        inMemoAnswerObj.roomId === answerObj.roomId &&
+        inMemoAnswerObj.sender_SocketId === answerObj.sender_SocketId
+      )
+    );
+    if (actualAnswer) {
       msg = 'answer is updated';
       actualAnswer.responded = true;
     }
@@ -174,13 +249,6 @@ io.on("connection", (socket) => {
     socket.broadcast.to(roomId).emit("msgTotoggleAudio", socket.id);
   });
 
-  // socket.on("replace_Peer",({roomId, offer, receiver_SocketId })=>{
-  //    console.log("replace_Peer");
-  //    const offerObj = {replaced : false,responded : false,roomId,offer,sender_SocketId:socket.id,receiver_SocketId};
-  //    offers.push(offerObj);
-  //    io.to(receiver_SocketId).emit("update_Peer",offerObj);
-  // });
-
   socket.on("intial_Mic_Or_Camera_Status", ({ kind, value, remoteSocketId }) => {
     console.log("intial_Mic_Or_Camera_Status");
     io.to(remoteSocketId).emit("receive_Intial_Mic_Or_Camera_Status", { kind, value, sender_SocketId: socket.id });
@@ -189,6 +257,43 @@ io.on("connection", (socket) => {
   socket.on("ask_For_Initial_Camera_Or_Mic_Status", ({ kind, remoteSocketId }) => {
     console.log("ask_For_Initial_Camera_Or_Mic_Status");
     io.to(remoteSocketId).emit("receive_Request_For_Inital_Media_Device_Status", { kind, sender_SocketId: socket.id });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("disconnect");
+
+    const participant = inMemoryParticipants.find(
+      inMemoParticipant => inMemoParticipant.socketId === socket.id
+    );
+
+    if (!participant) return;
+
+    const roomId = participant.roomId;
+
+    const roomParticipants = inMemoryParticipants.filter(
+      inMemoParticipant =>
+        inMemoParticipant.roomId === roomId &&
+        inMemoParticipant.socketId !== socket.id
+    );
+
+    inMemoryParticipants = inMemoryParticipants.filter(
+      inMemoParticipant => inMemoParticipant.socketId !== socket.id
+    );
+
+    inMemoryOffers = inMemoryOffers.filter(
+      inMemoOfferObj => inMemoOfferObj.sender_SocketId !== socket.id
+    );
+
+    inMemoryAnswers = inMemoryAnswers.filter(
+      inMemoAnswerObj => inMemoAnswerObj.sender_SocketId !== socket.id
+    );
+
+    socket.broadcast.to(roomId).emit("participantLeave", {
+      participants: roomParticipants,
+      leaveSocketId: socket.id
+    });
+
+    socket.leave(roomId);
   });
 
 });
