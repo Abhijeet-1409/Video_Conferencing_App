351429b607ead614ed39870d6bdc164ba9473bfe
Abhijeet-1409 <abhijeetjoy14@gmail.com>
2025-10-27 22:54:28 +0530

    add responsiveness to home page and animation to slideshow

--- client/src/components/slideShow/SlideShowComponent.jsx
+++ client/src/components/slideShow/SlideShowComponent.jsx
@@ -1,53 +1,113 @@
-//slideShowComponent.jsx
-
 import { useState } from 'react';
 import image1 from '../../assets/image1.jpg';
 import image2 from '../../assets/image2.jpg';
 import image3 from '../../assets/image3.avif';
 import style from './SlideShowComponent.module.css';
+import usePreloadImages from '../../hooks/usePreloadImages';
 
 export default function SlideShow() {
-    const [slideIndex, setSlideIndex] = useState(0);
+  const slides = [
+    {
+      imageSrc: image1,
+      title: 'See everyone together',
+      description:
+        'To see more people at the same time, go to Change layout in the More options menu',
+    },
+    {
+      imageSrc: image2,
+      title: 'Get a link you can share',
+      description:
+        'Click Create to get a link you can send to people you want to meet with',
+    },
+    {
+      imageSrc: image3,
+      title: 'Your meeting is safe',
+      description:
+        'No one outside your organization can join a meeting unless invited or admitted by the host',
+    },
+  ];
 
-    const slideShowContent = [
-        {
-            imageSrc: image1,
-            title: 'See everyone together',
-            description: 'To see more people at the same time, go to Change layout in the More options menu',
-        },
-        {
-            imageSrc: image2,
-            title: 'Get a link you can share',
-            description: 'Click Create to get a link you can send to people you want to meet with',
-        },
-        {
-            imageSrc: image3,
-            title: 'Your meeting is safe',
-            description: 'No one outside your organization can join a meeting unless invited or admitted by the host',
-        }
-    ];
+  const allLoaded = usePreloadImages(slides.map((s) => s.imageSrc));
+  const [index, setIndex] = useState(0);
+  const [isAnimating, setIsAnimating] = useState(false);
 
-    const prevSlide = () => {
-        setSlideIndex((slideIndex - 1 + slideShowContent.length) % slideShowContent.length);
-    };
+  const nextSlide = () => {
+    if (isAnimating) return;
+    setIsAnimating(true);
+    setTimeout(() => {
+      setIndex((prev) => (prev + 1) % slides.length);
+      setIsAnimating(false);
+    }, 400);
+  };
 
-    const nextSlide = () => {
-        setSlideIndex((slideIndex + 1) % slideShowContent.length);
-    };
+  const prevSlide = () => {
+    if (isAnimating) return;
+    setIsAnimating(true);
+    setTimeout(() => {
+      setIndex((prev) => (prev - 1 + slides.length) % slides.length);
+      setIsAnimating(false);
+    }, 400);
+  };
 
+  if (!allLoaded) {
     return (
-        <div className={style.gridContainer}>
-            <div className={style.gridButtonContainer}>
-                <button className={style.gridButton} onClick={prevSlide}>&lt;</button>
-            </div>
-            <div className={style.gridItem}>
-                <img src={slideShowContent[slideIndex].imageSrc} alt={slideShowContent[slideIndex].title} height={300} width={300} className={style.image}/>
-                <h2>{slideShowContent[slideIndex].title}</h2>
-                <p>{slideShowContent[slideIndex].description}</p>
-            </div>
-            <div className={style.gridButtonContainer}>
-                <button className={style.gridButton} onClick={nextSlide}>&gt;</button>
-            </div>
-        </div>
+      <div className={style.loadingContainer}>
+        <div className={style.spinner}></div>
+        <p>Loading slideshow...</p>
+      </div>
     );
+  }
+
+  const current = slides[index];
+
+  return (
+    <div className={style.gridContainer}>
+      {/* Left button (desktop/tablet) */}
+      <div className={style.gridButtonContainer}>
+        <button
+          className={style.gridButton}
+          onClick={prevSlide}
+          disabled={isAnimating}
+        >
+          &lt;
+        </button>
+      </div>
+
+      {/* Main content */}
+      <div className={`${style.gridItem} ${isAnimating ? style.fadeOut : style.fadeIn}`}>
+        <img src={current.imageSrc} alt={current.title} className={style.image} />
+        <h2>{current.title}</h2>
+        <p>{current.description}</p>
+      </div>
+
+      {/* Right button (desktop/tablet) */}
+      <div className={style.gridButtonContainer}>
+        <button
+          className={style.gridButton}
+          onClick={nextSlide}
+          disabled={isAnimating}
+        >
+          &gt;
+        </button>
+      </div>
+
+      {/* Mobile-only bottom buttons */}
+      <div className={style.mobileButtonBar}>
+        <button
+          className={style.gridButton}
+          onClick={prevSlide}
+          disabled={isAnimating}
+        >
+          &lt;
+        </button>
+        <button
+          className={style.gridButton}
+          onClick={nextSlide}
+          disabled={isAnimating}
+        >
+          &gt;
+        </button>
+      </div>
+    </div>
+  );
 }
